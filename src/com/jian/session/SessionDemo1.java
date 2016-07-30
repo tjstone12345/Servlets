4b3f504f024347b83408cd1fab27279df86f9fd5
Jian <tjapply@gmail.com>
2016-07-30 17:50:32 +0200

    add session

---
+++ src/com/jian/session/SessionDemo1.java
@@ -0,0 +1,31 @@
+package com.jian.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionDemo1
+ */
+//购买
+
+@WebServlet("/SessionDemo1")
+public class SessionDemo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.setAttribute("name", "洗衣机");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
