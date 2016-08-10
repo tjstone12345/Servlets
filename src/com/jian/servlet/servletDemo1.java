beed6ce693072a6d13933b30cad385fa88b8523f
Jian <tjapply@gmail.com>
2016-08-10 23:19:36 +0200

    update

--- src/com/jian/servlet/servletDemo1.java
+++ src/com/jian/servlet/servletDemo1.java
@@ -14,26 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletDemo1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletDemo1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		  ServletOutputStream out = response.getOutputStream(); 
 		  out.write("Hello Servelt".getBytes());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
